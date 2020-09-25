5c06a6d046a36ad8c11918bf7ac28f7b82c56512
oh29oh29 <hyukjae89@gmail.com>
2020-09-25 23:24:49 +0900

    jpa

---
+++ persistence-context/src/main/java/oh29oh29/PersistenceContextApplication.java
@@ -0,0 +1,13 @@
+package oh29oh29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceContextApplication.class, args);
+    }
+
+}
