c04922839f8e55896324abbe85e9329a018b4fe1
oh29oh29 <oh29oh29.develop@gmail.com>
2021-03-06 13:50:56 +0900

    [Spring-JPA] Transactional - 불필요한 주석 제거

--- transactional/src/main/java/oh29oh29/App01Runner.java
+++ transactional/src/main/java/oh29oh29/App01Runner.java
@@ -14,25 +14,15 @@ public class App01Runner implements ApplicationRunner {
 
     private final AAARepository aaaRepository;
 
-    public App01Runner(
-            AAARepository aaaRepository
-    ) {
+    public App01Runner(AAARepository aaaRepository) {
         this.aaaRepository = aaaRepository;
     }
 
-    /**
-     * 일반적인 엔티티와 Persistable 을 구현하여 isNew 를 추가한 엔티티에 대하여
-     * insert 와 update 의 의도를 갖고 구현했을 때
-     * 실제 쿼리는 어떻게 실행되는지 살펴보자.
-     * */
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
         System.out.println("\n===== Starting App01Runner =====");
 
-        /**
-         * 일반적인 엔티티에 대하여 insert 와 update
-         * */
         insertAAA();
         updateAAA();
     }
